610626cbcde72a1a24fecd2114adb9f9b201e998
Damien Leroy <damien7leroy@gmail.com>
2017-05-24 14:51:12 +0200

    Add class

--- Compression-LZW.py
+++ Compression-LZW.py
@@ -1,20 +1,32 @@
-#-*- coding:UTF-8 -*- 
-#Python 2.7 a ete utilise 
+#-*- coding:UTF-8 -*-
+#Python 2.7 a ete utilise
 
+class Compression-LZW:
 
+    def __init__(self, fileName):
+        """
+        Constructor
+        """
+        self.content = None
 
+    def readFile(self, fileName):
+        """
+        """
+        self.fileName = fileName
 
+    def writeFile(self):
+        """
+        """
 
-
-
- w = Nul;
-   for c in file:
-       if(w + c existe dans le dictionnaire) in list():
-           w = w + c;
-       else
-           ajouter w + c au dictionnaire;
-           écrire le code de w;
-           w = c;
-       fin si
-   fin tant que
-   écrire le code de w;
+    def compress(self):
+        w = Nul;
+        for c in file:
+           if(w + c existe dans le dictionnaire) in list():
+               w = w + c;
+           else
+               ajouter w + c au dictionnaire;
+               écrire le code de w;
+               w = c;
+           fin si
+        fin tant que
+        écrire le code de w;
