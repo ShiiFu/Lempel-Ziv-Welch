43dc2496f9d0f392378644f83b634fc5180306cf
Julien Yvenat <jul.y@orange.fr>
2017-05-24 14:47:00 +0200

    Algorithme implementation

--- Compression-LZW.py
+++ Compression-LZW.py
@@ -1,2 +1,20 @@
 #-*- coding:UTF-8 -*- 
 #Python 2.7 a ete utilise 
+
+
+
+
+
+
+
+ w = Nul;
+   for c in file:
+       if(w + c existe dans le dictionnaire) in list():
+           w = w + c;
+       else
+           ajouter w + c au dictionnaire;
+           écrire le code de w;
+           w = c;
+       fin si
+   fin tant que
+   écrire le code de w;
