5211f0f3ae8cf9f035a852179793aa18e4fc5e61
Damien Leroy <damien7leroy@gmail.com>
2017-05-31 10:15:53 +0200

    PEP8 the code

--- Compression-LZW.py
+++ Compression-LZW.py
@@ -1,10 +1,11 @@
-#-*- coding:UTF-8 -*-
-#Python 2.7 a ete utilise
+# -*- coding:UTF-8 -*-
+# Python 2.7 a ete utilise
 
 from array import array
 import argparse
 import sys
 import re
+
 
 class CompressionLZW:
 
@@ -60,14 +61,14 @@ class CompressionLZW:
         for i in range(0, len(binaryString), 9):
             self.compressed.append(int(binaryString[i:i+9], 2))
 
-
     def writeFileC(self, fileName):
         """
-        Fonction permettant d'écrire les valeurs compresser dans un fichier binaire.
+        Fonction permettant d'écrire les valeurs compresser dans un fichier
+        binaire.
         @param self doit avoir comme paramètre un objet CompressionLZW
         @type self CompressionLZW
         """
-        binaryString =  ""
+        binaryString = ""
         for i in self.compressed:
             binaryString += "{0:09b}".format(i)
 
@@ -79,39 +80,44 @@ class CompressionLZW:
         binaryArray.tofile(fileCompressed)
 
     def writeFileD(self, fileName):
-		"""
-		Fonction permettant d'écrire les valeurs décompresser dans un fichier texte standard.
-		@param self doit avoir comme paramètre un objet CompressionLZW
-		@type self CompressionLZW
-		"""
-		fileCompressed = open(fileName + ".txt", "wb")
-		fileCompressed.write(self.content)
+        """
+        Fonction permettant d'écrire les valeurs décompresser dans un fichier
+        texte standard.
+        @param self doit avoir comme paramètre un objet CompressionLZW
+        @type self CompressionLZW
+        """
+        fileCompressed = open(fileName + ".txt", "wb")
+        fileCompressed.write(self.content)
 
     def compress(self):
         """
-        Fonction pour compresser un fichier texte. Il utilise un système d'association entre charactères ASCII qui sont positionné dans un tableau.
-
+        Fonction pour compresser un fichier texte. Il utilise un système
+        d'association entre charactères ASCII qui sont positionné dans un
+        tableau.
         """
-        print "Compression du fichier", self.fileName,"\n"
+        print "Compression du fichier", self.fileName, "\n"
         w = ""
         for c in self.content:
-           if w + c in self.dictionnaire:
-               w = w + c
-           else:
-               if w != "" :
-                   self.dictionnaire.append(w + c)
-                   if len(w) > 1:
-                       self.compressed.append(self.dictionnaire.index(w) + 256)
-                   else:
-                       self.compressed.append(ord(w))
-               w = c
+            if w + c in self.dictionnaire:
+                w = w + c
+            else:
+                if w != "":
+                    self.dictionnaire.append(w + c)
+                    if len(w) > 1:
+                        self.compressed \
+                            .append(self.dictionnaire.index(w) + 256)
+                    else:
+                        self.compressed.append(ord(w))
+                w = c
 
     def decompress(self):
         """
-        Fonction pour décompresser un fichier binaire. Il lis la valeur binaire (entre 256 et +++) pour pouvoir remplacer ce code par sa valeur il prend la valeur ou l'index
+        Fonction pour décompresser un fichier binaire. Il lis la valeur binaire
+        (entre 256 et +++) pour pouvoir remplacer ce code par sa valeur il
+        prend la valeur ou l'index
         de la liste et 256 - n ,  n étant la valeur binaire.
         """
-        print "Décompression du fichier", self.fileName,"\n"
+        print "Décompression du fichier", self.fileName, "\n"
         w = chr(self.compressed[0])
         self.dictionnaire = []
         self.content = w
@@ -122,23 +128,24 @@ class CompressionLZW:
                 else:
                     ent = w + w[0]
             else:
-               ent = chr(c)
+                ent = chr(c)
             self.dictionnaire.append(w + ent[0])
             self.content += ent
             w = ent
 
 
 def Compression(infile, outfile):
-	fileD = CompressionLZW()
-	fileD.readFileText(infile)
-	fileD.compress()
-	fileD.writeFileC(outfile)
+    fileD = CompressionLZW()
+    fileD.readFileText(infile)
+    fileD.compress()
+    fileD.writeFileC(outfile)
+
 
 def Decompression(infile, outfile):
-	fileD = CompressionLZW()
-	fileD.readFileBin(infile)
-	fileD.decompress()
-	fileD.writeFileD(outfile)
+    fileD = CompressionLZW()
+    fileD.readFileBin(infile)
+    fileD.decompress()
+    fileD.writeFileD(outfile)
 
 
 if __name__ == '__main__':
@@ -146,25 +153,26 @@ if __name__ == '__main__':
     Documentation interne disponible écrite ci-dessous.
     Pour la lire exécutez le programme avec l'argument argument -h
     """
-    parser = argparse.ArgumentParser(description='Compresse ou Décompresse un fichier avec la méthode Lempel-Ziv-Welch')
+    parser = argparse.ArgumentParser(description="Compresse ou Décompresse "
+                                                 "un fichier avec la méthode "
+                                                 "Lempel-Ziv-Welch")
     parser.add_argument('infile', nargs=1,
-    					help='Nom du fichier en entrée')
+                        help='Nom du fichier en entrée')
     parser.add_argument('outfile', nargs='?',
-    					help='Nom du fichier en sortie')
+                        help='Nom du fichier en sortie')
     parser.add_argument("-d", dest='Decompression', action="store_true",
-    					help='Décompresse le fichier en entrée')
+                        help='Décompresse le fichier en entrée')
     parser.add_argument("-c", dest='Compression', action="store_true",
-    					help='Compresse le fichier en entrée')
-
+                        help='Compresse le fichier en entrée')
 
     args = parser.parse_args()
 
-    m = re.split("'",str(args.infile))
+    m = re.split("'", str(args.infile))
     infile = str(m[1])
 
-    if args.Compression==True and args.Decompression==False:
+    if args.Compression is True and args.Decompression is False:
     	Compression(infile, args.outfile)
-    elif args.Decompression==True and args.Compression==False:
-    	Decompression(infile, args.outfile)
+    elif args.Decompression is True and args.Compression is False:
+        Decompression(infile, args.outfile)
     else:
-    	print("Effectuez la commande -h pour l'aide")
+        print("Effectuez la commande -h pour l'aide")
